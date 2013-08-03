59f5ca5e269e002b48280871b6105e956c403bf9
TuxCraft <tux.craft.42@gmail.com>
2013-08-03 10:21:13 -0400

    Fixed spawning & giant item work

--- src/main/java/com/github/ubiquitousspice/dreamdimension/DreamDimension.java
+++ src/main/java/com/github/ubiquitousspice/dreamdimension/DreamDimension.java
@@ -1,11 +1,25 @@
 package com.github.ubiquitousspice.dreamdimension;
 
+import java.util.logging.Logger;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityEggInfo;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraftforge.common.Configuration;
+import net.minecraftforge.common.DimensionManager;
+
 import com.github.ubiquitousspice.dreamdimension.blocks.BlockBouncy;
 import com.github.ubiquitousspice.dreamdimension.blocks.BlockCheatyPortal;
 import com.github.ubiquitousspice.dreamdimension.dimension.WorldProviderMod;
 import com.github.ubiquitousspice.dreamdimension.entities.EntityGiantItem;
 import com.github.ubiquitousspice.dreamdimension.entities.EntityLargeSheep;
 import com.github.ubiquitousspice.dreamdimension.world.BiomeGenDream;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
@@ -26,6 +40,7 @@ import net.minecraftforge.common.DimensionManager;
 import java.util.logging.Logger;
 
 //import com.github.ubiquitousspice.dreamdimension.blocks.BlockDreamDirt;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid = DreamDimension.MODID, version = DreamDimension.VERSION, name = "The Dream Dimension")
 public class DreamDimension
@@ -146,4 +161,127 @@ public class DreamDimension
 
         EntityList.entityEggs.put(Integer.valueOf(id), new EntityEggInfo(id, bgColor, fgColor));
     }
+public class DreamDimension {
+	public static final String MODID = "dreamdimension";
+	public static final String VERSION = "0.1";
+
+	@Mod.Instance
+	public static DreamDimension instance;
+
+	@SidedProxy(modId = MODID, clientSide = "com.github.ubiquitousspice.dreamdimension.client.ProxyClient", serverSide = "com.github.ubiquitousspice.dreamdimension.ProxyCommon")
+	public static ProxyCommon proxy;
+
+	public static Logger logger;
+
+	// Material
+	public static Material material;
+
+	// random configurations
+	public static boolean dreamMaterialBreakable = false;
+
+	// dimension configs
+
+	// IDS
+	public static int dimensionID;
+	static int startEntityId = 300;
+
+	private int idDreamDirt;
+	private int idDLauncher;
+
+	// blocks
+	public static Block dreamDirt;
+	public static Block bouncyBlock;
+
+	private int idPortalBlock;
+	public static Block portalBlock;
+
+	public static BiomeGenBase dreamy;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		// get logger
+		logger = event.getModLog();
+
+		// mess with material
+		material = (new MaterialDream());
+
+		// CONFIGURATION STUFF
+		{
+			Configuration config = new Configuration(
+					event.getSuggestedConfigurationFile());
+			idDreamDirt = config.getBlock("DreamDirt", 300).getInt();
+			idPortalBlock = config.getBlock("PortalBlock", 301).getInt();
+			dimensionID = config.get(Configuration.CATEGORY_GENERAL,
+					"Dream Dimension Idea", 2).getInt();
+
+			// config blockIDs
+			int baseid = 300;
+			idDreamDirt = config.getBlock("DreamDirt", baseid++).getInt();
+			idDLauncher = config.getBlock("DreamLauncher", baseid++).getInt();
+			if (config.hasChanged())
+				config.save();
+
+			// config itemIDs
+
+			// config dimension
+
+			// config other
+			dreamMaterialBreakable = config.get("Adventure",
+					"dreamMaterialBreakable", false).getBoolean(false);
+
+			// save it.
+			if (config.hasChanged()) {
+				config.save();
+			}
+		}
+		// do config stuff
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		// do blocks and stuff here.
+		dreamDirt = new Block(idDreamDirt, material).setUnlocalizedName(
+				MODID + ".dreamDirt").func_111022_d(MODID + ":dreamDirt");
+		bouncyBlock = new BlockBouncy(idDLauncher).setUnlocalizedName(
+				MODID + ".dreamLauncher").func_111022_d(
+				MODID + ":dreamLauncher");
+
+		// registrations
+		GameRegistry.registerBlock(dreamDirt, "dreamDirt");
+		GameRegistry.registerBlock(bouncyBlock, "dreamLauncher");
+
+		portalBlock = new BlockCheatyPortal(idPortalBlock)
+				.setUnlocalizedName(MODID + ".portalBlock");
+		GameRegistry.registerBlock(portalBlock, "portalBlock");
+
+		dreamy = new BiomeGenDream(25);
+
+		DimensionManager.registerProviderType(dimensionID,
+				WorldProviderMod.class, true);
+		DimensionManager.registerDimension(dimensionID, dimensionID);
+		
+		registerEntity(EntityLargeSheep.class, "LargeSheep", 15198183, 16758197);
+		LanguageRegistry.instance().addStringLocalization(
+				"entity.dreamdimension.LargeSheep.name", "King Lambchop");
+		
+		registerEntity(EntityGiantItem.class, "GiantItem");
+		
+		proxy.registerRenderers();
+	}
+	
+	public void registerEntity(Class<? extends Entity> entityClass, String entityName, int bkEggColor, int fgEggColor)
+	  {
+	    int id = EntityRegistry.findGlobalUniqueEntityId();
+
+	    EntityRegistry.registerGlobalEntityID(entityClass, entityName, id);
+
+	    EntityList.entityEggs.put(Integer.valueOf(id), new EntityEggInfo(id, fgEggColor, bkEggColor));
+	  }
+	
+	public void registerEntity(Class<? extends Entity> entityClass, String entityName)
+	  {
+	    int id = EntityRegistry.findGlobalUniqueEntityId();
+
+	    EntityRegistry.registerGlobalEntityID(entityClass, entityName, id);
+	  }
 }
--- src/main/java/com/github/ubiquitousspice/dreamdimension/client/ProxyClient.java
+++ src/main/java/com/github/ubiquitousspice/dreamdimension/client/ProxyClient.java
@@ -1,5 +1,6 @@
 package com.github.ubiquitousspice.dreamdimension.client;
 
+import com.github.ubiquitousspice.dreamdimension.DreamDimension;
 import com.github.ubiquitousspice.dreamdimension.ProxyCommon;
 import com.github.ubiquitousspice.dreamdimension.entities.EntityGiantItem;
 import com.github.ubiquitousspice.dreamdimension.entities.EntityLargeSheep;
@@ -9,6 +10,7 @@ import com.github.ubiquitousspice.dreamdimension.entities.RenderGiantItem;
 import com.github.ubiquitousspice.dreamdimension.entities.RenderLargeSheep;
 
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.common.registry.EntityRegistry;
 
 public class ProxyClient extends ProxyCommon
 {
@@ -16,7 +18,8 @@ public class ProxyClient extends ProxyCommon
     public void registerRenderers()
     {
         RenderingRegistry.registerEntityRenderingHandler(EntityLargeSheep.class, new RenderLargeSheep(new ModelLargeSheep2(), new ModelLargeSheep(), 0.7F));
-
-        RenderingRegistry.registerEntityRenderingHandler(EntityGiantItem.class, new RenderGiantItem());
+        
+		RenderingRegistry.registerEntityRenderingHandler(EntityGiantItem.class,
+				new RenderGiantItem());
     }
 }
